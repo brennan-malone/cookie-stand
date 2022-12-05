0bde135b05b23131274e36e5704460968e743a96
Brennan Malone <malone.brennan98@gmail.com>
2022-12-05 17:14:37 -0500

    Added in constructor for creating new locations

--- js/app.js
+++ js/app.js
@@ -8,16 +8,27 @@
 
 // Display the values of each array as unordered lists in the browser.
 
-// Calculating the sum of these hourly totals; your output for each location should look like this:
+const hoursOfOperation = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
 
-function Location(cookiesPerCust, maxHourCust, minHourCust) {
+function Location(name, cookiesPerCust, maxHourCust, minHourCust, total) {
+  this.name = name;
   this.cookiesPerCust = cookiesPerCust;
   this.maxHourCust = maxHourCust;
   this.minHourCust = minHourCust;
-  function randCustPerHour(){
+  function randCustPerHour() {
     return Math.floor(Math.random() * (maxHourCust - minHourCust) + minHourCust);
   }
-  this.cookiesPerHour = randCustPerHour() * cookiesPerCust;
-  
-
+  this.totalCookies = 0;
+  this.cookiePerHour = [];
+  this.total = total;
+  function hourlyCookies() {
+    for (let i = 0; i < hoursOfOperation.length; i++) {
+      let aveCookie = Math.floor(this.cookiesPerCust * this.randCustPerHour());
+      this.cookiePerHour.push(aveCookie)
+      this.total += aveCookie;
+    }
+  };
 }
+let Paris = new Location('Paris', 2.3, 38, 20);
+
+console.log(Paris);
